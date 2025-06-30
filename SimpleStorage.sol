d1f86044cbc3c088d9f5bf074588bb1214f4536f
Rishi <kekonline@gmail.com>
2025-06-30 16:36:20 +0200

    refactor: simplify SimpleStorage contract and add ExtraStorage and StorageFactory contracts

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,40 +1,33 @@
+// I'm a comment!
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.30;
 
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
+
 contract SimpleStorage {
-    // bool flag = false;
-    // uint256 num = 1974 ;
-    // string mesg ="hello world" ;
-    // address payeeAddress =  address(0);
-
-    uint256 public favoriteNumber;
-    People public person =
-        People({favoriteNumber: 5, name: "Tom", married: false});
-    People[] public people;
-
-    mapping(string => uint256) public nameToFavoriteNumber;
+    uint256 favoriteNumber;
 
     struct People {
         uint256 favoriteNumber;
         string name;
-        bool married;
     }
+    // uint256[] public anArray;
+    People[] public people;
 
-    function addPerson(
-        string memory _name,
-        uint256 _favoriteNumber,
-        bool _married
-    ) public {
-        People memory _person = People({
-            name: _name,
-            favoriteNumber: _favoriteNumber,
-            married: _married
-        });
-        people.push(_person);
-        nameToFavoriteNumber[_name] = _person.favoriteNumber;
-    }
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
